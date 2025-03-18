1037290ab81f4f9854e416ddb9c758087720dc42
Zyverx <germangallegogarcia1999@gmail.com>
2025-03-18 19:47:07 +0100

    Ejercicio Acabado
    Se ha pasado a PDF todo el Código (Usando el individuo correcto)

--- ElectroCardiograma/Code.R
+++ ElectroCardiograma/Code.R
@@ -1,5 +1,8 @@
 rm(list=ls())
 
+
+pacman::p_load(tidyverse,pracma,ggplot2)
+
 ###########################
 ### Leer el archivo hea ###
 ###########################
@@ -58,7 +61,6 @@ ECG_data <- data.frame(Time = seq(0, length(ECG_mV) - 1) / freq,  # Tiempo en se
 ##### Graficar ####
 ####################
 
-library(ggplot2)
 
 # Graficar la señal ECG
 ggplot(ECG_data, aes(x = Time, y = ECG)) +
@@ -71,8 +73,6 @@ ggplot(ECG_data, aes(x = Time, y = ECG)) +
 ####### DETECTAR PICOS R ########
 #################################
 
-install.packages("pracma")  
-library(pracma)
 # Definir un umbral basado en la señal ECG (puedes ajustarlo según la amplitud de tu señal)
 umbral <- max(ECG_mV) * 0.5  # La mitad del pico más alto
 
@@ -139,6 +139,10 @@ ggplot(RR_data, aes(x = Time, y = RR_Interval)) +
 #####################################
 
 
+##############################################
+### Método del Rango Intercuartílico (IQR) ###
+##############################################
+
 # Calcular los cuartiles
 Q1 <- quantile(ECG_mV, 0.25)
 Q3 <- quantile(ECG_mV, 0.75)
@@ -149,7 +153,7 @@ limite_inferior <- Q1 - 1.5 * IQR
 limite_superior <- Q3 + 1.5 * IQR
 
 # Detectar los valores anómalos
-outliers_IQR <- which(ECG_mV < limite_inferior | ECG_mV > limite_superior)
+outliers_IQR <- subset(ECG_mV, ECG_mV < limite_inferior | ECG_mV > limite_superior)
 
 # Mostrar los valores anómalos detectados
 outliers_IQR_values <- ECG_mV[outliers_IQR]
@@ -167,7 +171,59 @@ ggplot(ECG_data, aes(x = Time, y = ECG_mV)) +
   theme_minimal()
 
 
+###########################
+###  Método del Z-Score ###
+###########################
+
+# Calcular la media y la desviación estándar
+media <- mean(ECG_mV)
+desviacion_estandar <- sd(ECG_mV)
+
+# Calcular el Z-score para cada valor
+z_scores <- (ECG_mV - media) / desviacion_estandar
+
+# Definir el umbral de Z-score (por ejemplo, 3)
+outliers_Z <- which(abs(z_scores) > 3)
+
+# Mostrar los valores anómalos detectados
+outliers_Z_values <- ECG_mV[outliers_Z]
+outliers_Z_times <- ECG_data$Time[outliers_Z]
+
+# Mostrar los valores y tiempos de los outliers
+data.frame(Time = outliers_Z_times, ECG_mV = outliers_Z_values)
+
+ggplot(ECG_data, aes(x = Time, y = ECG_mV)) +
+  geom_line(color = "blue") +  # Señal ECG
+  geom_point(data = data.frame(Time = outliers_Z_times, ECG_mV = outliers_Z_values),
+             aes(x = Time, y = ECG_mV), color = "red", size = 2) +  # Outliers en rojo
+  labs(title = "Señal ECG con Outliers detectados (IQR)", x = "Tiempo (s)", y = "Amplitud (mV)") +
+  theme_minimal()
 
 
+##################################
+###  Método de los Percentiles ###
+##################################
+
+# Calcular los percentiles 2.5% y 97.5%
+percentil_2_5 <- quantile(ECG_mV, 0.025)
+percentil_97_5 <- quantile(ECG_mV, 0.975)
+
+# Detectar los valores anómalos
+outliers_percentiles <- which(ECG_mV < percentil_2_5 | ECG_mV > percentil_97_5)
+
+# Mostrar los valores anómalos detectados
+outliers_percentiles_values <- ECG_mV[outliers_percentiles]
+outliers_percentiles_times <- ECG_data$Time[outliers_percentiles]
+
+# Mostrar los valores y tiempos de los outliers
+data.frame(Time = outliers_percentiles_times, ECG_mV = outliers_percentiles_values)
+
+
+ggplot(ECG_data, aes(x = Time, y = ECG_mV)) +
+  geom_line(color = "blue") +  # Señal ECG
+  geom_point(data = data.frame(Time = outliers_percentiles_times, ECG_mV = outliers_percentiles_values),
+             aes(x = Time, y = ECG_mV), color = "red", size = 1) +  # Outliers en rojo
+  labs(title = "Señal ECG con Outliers detectados (IQR)", x = "Tiempo (s)", y = "Amplitud (mV)") +
+  theme_minimal()
 
 
